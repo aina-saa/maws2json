9831f1cb6b098640729d3192db98e023549bb7cf
Antti Peltonen <antti.peltonen@iki.fi>
2022-01-06 20:15:43 +0000

    version etc info in own subpackage updated on build time

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+version="0.0.1"
+time=$(date)
+hash=$(git log -n 1 --pretty=format:"%H")
+hostname=$(hostname)
+go build -ldflags="\
+    -X 'github.com/aina-saa/maws2json/version.BuildVersion=$version' \
+    -X 'github.com/aina-saa/maws2json/version.BuildTime=$time' \
+    -X 'github.com/aina-saa/maws2json/version.BuildHost=$hostname' \
+    -X 'github.com/aina-saa/maws2json/version.BuildSha=$hash'" .
+./maws2json --version
+
+# eof
--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/alecthomas/kong"
 
 	"github.com/aina-saa/maws2json/process"
+	"github.com/aina-saa/maws2json/version"
 )
 
+type VersionFlag string
+
+func (v VersionFlag) Decode(ctx *kong.DecodeContext) error { return nil }
+func (v VersionFlag) IsBool() bool                         { return true }
+func (v VersionFlag) BeforeApply(app *kong.Kong, vars kong.Vars) error {
+	info := fmt.Sprintf("%s ver. %s\n %s built at %s on %s\n Author: %s", app.Model.Name, version.BuildVersion, version.BuildSha, version.BuildTime, version.BuildHost, version.Author)
+	fmt.Println(info)
+	app.Exit(0)
+	return nil
+}
+
 var CLI struct {
-	File      string `help:"Input file or '-' for stdin" name:"file" type:"existingfile" short:"f" default:"-"`
-	Timestamp bool   `help:"Add timestamp to output." short:"t" negatable:"true" default:"true"`
-	Wind      bool   `help:"Process WIND messages." negatable:"true" default:"true"`
-	Log       bool   `help:"Process LOG messages." negatable:"true" default:"true"`
-	Ptu       bool   `help:"Process PTU messages." negatable:"true" default:"true"`
+	File      string      `help:"Input file or '-' for stdin" name:"file" type:"existingfile" short:"f" default:"-"`
+	Timestamp bool        `help:"Add timestamp to output." short:"t" negatable:"true" default:"true"`
+	Wind      bool        `help:"Process WIND messages." negatable:"true" default:"true"`
+	Log       bool        `help:"Process LOG messages." negatable:"true" default:"true"`
+	Ptu       bool        `help:"Process PTU messages." negatable:"true" default:"true"`
+	Version   VersionFlag `name:"version" help:"Print version information and quit"`
 }
 
 func main() {
